a0d9288a07e66bcf361ae8fbf463bef0d5b50350
Arsalan831 <arsalanbashir831@gmail.com>
2025-05-28 13:10:30 +0500

    remove extra files

--- temp/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = mongoose.Schema({
-    userId:String,
-    user_role:String,
-    password:String
-})
-
-const User = mongoose.model("User",userSchema);
-module.exports = User
