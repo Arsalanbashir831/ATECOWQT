18302f313f559a55c8abd4df73308ee41043f124
Saadaan-Hassan <webmaker9d@gmail.com>
2025-09-15 11:11:31 +0500

    Refactor Aasia Steel Card model and views to enhance welding variable fields. Updated schema to include detailed specifications for welding processes, backing, diameter, and more. Revamped forms in edit, insert, and view templates for improved clarity and consistency in data entry and display.

--- models/aasiaSteelCardModel.js
+++ models/aasiaSteelCardModel.js
@@ -16,18 +16,42 @@ const aasiaSteelCardSchema = mongoose.Schema({
     card_no: String,
     authorized_by: String,
     welding_inspector: String,
-    welding_process_and_type: String,
-    direct_or_remote_visual_control: String,
-    auto_arc_voltage_control: String,
-    automatic_joint_tracking: String,
-    positions: String,
-    base_material_dia_and_thickness: String,
-    consumable_inserts: String,
-    backing: String,
-    single_or_multiple_passes: String,
-    base_metal_spec_or_p_no: String,
-    filler_metal_spec_or_class: String,
-    joint_or_weld_type: String,
+    // Welding Process/Type
+    welding_process_type_actual: String,
+    welding_process_type_range: String,
+    // Backing
+    backing_actual: String,
+    backing_range: String,
+    // Diameter
+    diameter_actual: String,
+    diameter_range: String,
+    // Base Metal P-No.
+    base_metal_pno_actual: String,
+    base_metal_pno_range: String,
+    // Deposited Thickness
+    deposited_thickness_actual: String,
+    deposited_thickness_range: String,
+    // Position & Prog.
+    position_prog_actual: String,
+    position_prog_range: String,
+    // Filler Metal Class
+    filler_metal_class_actual: String,
+    filler_metal_class_range: String,
+    // Filler Metal F-No.
+    filler_metal_fno_actual: String,
+    filler_metal_fno_range: String,
+    // Use of Backing Gas
+    backing_gas_actual: String,
+    backing_gas_range: String,
+    // Transfer Mode
+    transfer_mode_actual: String,
+    transfer_mode_range: String,
+    // Current Type/Polarity
+    current_type_polarity_actual: String,
+    current_type_polarity_range: String,
+    // Join/Weld Type
+    join_weld_type_actual: String,
+    join_weld_type_range: String,
     image: String,
     qr: String
 });
